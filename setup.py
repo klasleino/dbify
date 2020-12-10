1199d4ffda3000e489b35b6a23a6fe960350461c
Klas Leino <kleino@live.com>
2020-12-10 15:03:27 -0500

    installation

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dbify',
+    version='0.0.1',
+    packages=find_packages(),
+    description='decorator for storing function results in a database'
+)
